a9b2d6946697ebeed98596ba7e7be503ce814d5d
Linklancien <linklancien@gmail.com>
2025-03-29 16:56:47 +0100

    upadate angles constraints

--- proc_anim/chain_and_anchor.v
+++ proc_anim/chain_and_anchor.v
@@ -36,7 +36,7 @@ pub interface User{
 pub fn (chain Chain) front_go_to(mut user User, cible vec.Vec2[f32]){
 	chain.front_to_back_update_pos(mut user, cible, false)
 
-	chain.front_to_back_update_angle(mut user)
+	chain.front_to_back_update_angle(mut user, cible)
 }
 
 pub fn (chain Chain) fabrik(mut user User, cible vec.Vec2[f32]){
@@ -105,7 +105,19 @@ pub fn (chain Chain) back_to_front_update_pos(mut user User, cible vec.Vec2[f32]
 
 
 // Contraintes de positions selon l'angles avec les autres maillons
-pub fn (chain Chain) front_to_back_update_angle(mut user User){
+pub fn (chain Chain) front_to_back_update_angle(mut user User, cible vec.Vec2){
+	pos := user.list_anchor[chain.body_anchor_index[0]].pos
+	prec_angle := pos.angle_towards(cible)
+	angle := pos.angle_towards(user.list_anchor[chain.body_anchor_index[1]].pos)
+	dif := angle - prec_angle
+	if math.abs(dif) < chain.angle_minimum  {
+		if dif > 0{
+			user.list_anchor[chain.body_anchor_index[1]].pos = pos + vec.vec2(f32(chain.vert_radius), f32(0.0)).rotate_around_ccw(vec.vec2(f32(0.0), f32(0.0)), prec_angle + chain.angle_minimum)
+		}
+		else if dif < 0{
+			user.list_anchor[chain.body_anchor_index[1]].pos = pos + vec.vec2(f32(chain.vert_radius), f32(0.0)).rotate_around_ccw(vec.vec2(f32(0.0), f32(0.0)), prec_angle - chain.angle_minimum)
+		}
+	}
 	
 	for index in 1..chain.body_anchor_index.len - 1{
 		pos := user.list_anchor[chain.body_anchor_index[index]].pos
@@ -125,21 +137,33 @@ pub fn (chain Chain) front_to_back_update_angle(mut user User){
 
 
 
-pub fn (chain Chain) back_to_front_update_angle(mut user User){
+pub fn (chain Chain) back_to_front_update_angle(mut user User , cible vec.Vec2){
+	pos := user.list_anchor[chain.body_anchor_index[chain.body_anchor_index.len - 1]].pos
+	prec_angle := pos.angle_towards(cible)
+	angle := pos.angle_towards(user.list_anchor[chain.body_anchor_index[chain.body_anchor_index.len - 2]].pos)
+	dif := angle - prec_angle
+	if math.abs(dif) < chain.angle_minimum  {
+		if dif > 0{
+			user.list_anchor[chain.body_anchor_index[chain.body_anchor_index.len - 2]].pos = pos + vec.vec2(f32(chain.vert_radius), f32(0.0)).rotate_around_ccw(vec.vec2(f32(0.0), f32(0.0)), prec_angle + chain.angle_minimum)
+		}
+		else if dif < 0{
+			user.list_anchor[chain.body_anchor_index[chain.body_anchor_index.len - 2]].pos = pos + vec.vec2(f32(chain.vert_radius), f32(0.0)).rotate_around_ccw(vec.vec2(f32(0.0), f32(0.0)), prec_angle - chain.angle_minimum)
+		}
+	}
 	
 	for index_revers in 2..chain.body_anchor_index.len - 1{
 		index := chain.body_anchor_index.len - index_revers
 
 		pos := user.list_anchor[chain.body_anchor_index[index]].pos
-		prec_angle := pos.angle_towards(user.list_anchor[chain.body_anchor_index[index + 1]].pos)
-		mut angle := pos.angle_towards(user.list_anchor[chain.body_anchor_index[index - 1]].pos)
-
-		if math.abs(angle) - math.abs(prec_angle) <= chain.angle_minimum{
-			if angle > math.pi{
-				user.list_anchor[chain.body_anchor_index[index - 1]].pos = pos + vec.vec2(f32(chain.vert_radius), f32(0.0)).rotate_around_ccw(vec.vec2(f32(0.0), f32(0.0)), -chain.angle_minimum + math.abs(prec_angle))
+		prec_angle := pos.angle_towards(user.list_anchor[chain.body_anchor_index[index - 1]].pos)
+		angle := pos.angle_towards(user.list_anchor[chain.body_anchor_index[index + 1]].pos)
+		dif := angle - prec_angle
+		if math.abs(dif) < chain.angle_minimum  {
+			if dif > 0{
+				user.list_anchor[chain.body_anchor_index[index - 1]].pos = pos + vec.vec2(f32(chain.vert_radius), f32(0.0)).rotate_around_ccw(vec.vec2(f32(0.0), f32(0.0)), prec_angle + chain.angle_minimum)
 			}
-			else{
-				user.list_anchor[chain.body_anchor_index[index - 1]].pos = pos + vec.vec2(f32(chain.vert_radius), f32(0.0)).rotate_around_ccw(vec.vec2(f32(0.0), f32(0.0)), chain.angle_minimum + math.abs(prec_angle))
+			else if dif < 0{
+				user.list_anchor[chain.body_anchor_index[index - 1]].pos = pos + vec.vec2(f32(chain.vert_radius), f32(0.0)).rotate_around_ccw(vec.vec2(f32(0.0), f32(0.0)), prec_angle - chain.angle_minimum)
 			}
 		}
 		
