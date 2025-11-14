303c4bf9cbb07d03e917cf7ea2924747b5e25d82
Nopana_Eliyaan <nopana1007@gmail.com>
2025-11-15 00:20:02 +0100

    fmt, fix, fix indices, replace f32 with f64

--- chain_and_anchor.v
+++ chain_and_anchor.v
@@ -3,40 +3,64 @@ module proc_anim
 import math
 import math.vec
 
-const origin = vec.vec2(f32(0.0), f32(0.0))
+const origin = vec.vec2(f64(0.0), f64(0.0))
 
-// pos_constraints.len = points.len - 1
-// min/max_constraints.len = points.len - 2
-pub fn front_to_back_min_max(mut points []vec.Vec2[f32], pos_constraints []f32, min_constraints []f32, max_constraints []f32) {
+// pos_constraints.len = points.len - 1 (0th is for the link between 0th and 1st point)
+// min/max_constraints.len = points.len - 2 (0th is for the 1st point)
+pub fn front_to_back_min_max(mut points []vec.Vec2[f64], pos_constraints []f64, min_constraints []f64, max_constraints []f64) {
 	for ip in 0 .. points.len - 2 {
-		points[ip] = apply_pos_constraint(points[ip], points[ip + 1], pos_constraints[ip])
-		points[ip] = apply_min_max_angle_constraints(points[ip], points[ip + 1], points[ip + 2],
+		points[ip] = apply_pos_constraint(points[ip + 1], points[ip], pos_constraints[ip])
+		points[ip] = apply_min_max_angle_constraint(points[ip + 2], points[ip + 1], points[ip],
 			min_constraints[ip], max_constraints[ip])
 	}
-	points[ip] = apply_pos_constraint(points[points.len - 2], points[points.len - 1],
+	points[points.len - 2] = apply_pos_constraint(points[points.len - 1], points[points.len - 2],
 		pos_constraints[points.len - 2])
 }
 
-// pos_constraints.len = points.len - 1
-// angle_constraints.len = points.len - 2
-pub fn front_to_back_min_max(mut points []vec.Vec2[f32], pos_constraints []f32, angle_constraints []f32) {
+// pos_constraints.len = points.len - 1 (0th is for the link between 0th and 1st point)
+// min/max_constraints.len = points.len - 2 (0th is for the 1st point)
+pub fn back_to_front_min_max(mut points []vec.Vec2[f64], pos_constraints []f64, min_constraints []f64, max_constraints []f64) {
+	p := points.len - 1
 	for ip in 0 .. points.len - 2 {
-		points[ip] = apply_pos_constraint(points[ip], points[ip + 1], pos_constraints[ip])
-		points[ip] = apply_angle_constraints(points[ip], points[ip + 1], points[ip + 2],
+		points[p - ip] = apply_pos_constraint(points[p - (ip + 1)], points[p - ip], pos_constraints[p - ip - 1])
+		points[p - ip] = apply_min_max_angle_constraint(points[p - (ip + 2)], points[p - (ip + 1)],
+			points[p - ip], min_constraints[p - ip - 2], max_constraints[p - ip - 2])
+	}
+	points[1] = apply_pos_constraint(points[0], points[1], pos_constraints[0])
+}
+
+// pos_constraints.len = points.len - 1 (0th is for the link between 0th and 1st point)
+// angle_constraints.len = points.len - 2 (0th is for the 1st point)
+pub fn front_to_back(mut points []vec.Vec2[f64], pos_constraints []f64, angle_constraints []f64) {
+	for ip in 0 .. points.len - 2 {
+		points[ip] = apply_pos_constraint(points[ip + 1], points[ip], pos_constraints[ip])
+		points[ip] = apply_angle_constraint(points[ip + 2], points[ip + 1], points[ip],
 			angle_constraints[ip])
 	}
-	points[ip] = apply_pos_constraint(points[points.len - 2], points[points.len - 1],
+	points[points.len - 2] = apply_pos_constraint(points[points.len - 1], points[points.len - 2],
 		pos_constraints[points.len - 2])
+}
+
+// pos_constraints.len = points.len - 1 (0th is for the link between 0th and 1st point)
+// angle_constraints.len = points.len - 2 (0th is for the 1st point)
+pub fn back_to_front(mut points []vec.Vec2[f64], pos_constraints []f64, angle_constraints []f64) {
+	p := points.len - 1
+	for ip in 0 .. points.len - 2 {
+		points[p - ip] = apply_pos_constraint(points[p - (ip + 1)], points[p - ip], pos_constraints[p - ip - 1])
+		points[p - ip] = apply_angle_constraint(points[p - (ip + 2)], points[p - (ip + 1)],
+			points[p - ip], angle_constraints[p - ip - 2])
+	}
+	points[1] = apply_pos_constraint(points[0], points[1], pos_constraints[0])
 }
 
 // returns the position of b constrained to (b - a).magnitude = constraint
-pub fn apply_pos_constraint(a vec.Vec2[f32], b vec.Vec2[f32], constraint f32) vec.Vec2[f32] {
+pub fn apply_pos_constraint(a vec.Vec2[f64], b vec.Vec2[f64], constraint f64) vec.Vec2[f64] {
 	return a + b.unit().mul_scalar(constraint)
 }
 
 // returns the furthest position for the new `c` point (but blocks the angle <`a` `b` `c`> at `constraint` (radians)
 // returns the new position for that does not break the angle constraint
-pub fn apply_angle_constraint(a vec.Vec2[f32], b vec.Vec2[f32], c vec.Vec2[f32], constraint f32) vec.Vec2[f32] {
+pub fn apply_angle_constraint(a vec.Vec2[f64], b vec.Vec2[f64], c vec.Vec2[f64], constraint f64) vec.Vec2[f64] {
 	ab := b - a
 	bc := c - b
 	wanted_angle := valid_angle(ab.angle_between(bc))
@@ -52,13 +76,12 @@ pub fn apply_angle_constraint(a vec.Vec2[f32], b vec.Vec2[f32], c vec.Vec2[f32],
 // returns the furthest position for the new `c` point but blocks the angle `min_constraint` < <`a` `b` `c`> < `max_constraint`  (radians)
 // returns the new position for c that does not break the angle constraint
 // used for asymetric angle constraint
-pub fn apply_min_max_angle_constraint(a vec.Vec2[f32], b vec.Vec2[f32], c vec.Vec2[f32], _min_constraint f32, _max_constraint f32) vec.Vec2[f32] {
+pub fn apply_min_max_angle_constraint(a vec.Vec2[f64], b vec.Vec2[f64], c vec.Vec2[f64], _min_constraint f64, _max_constraint f64) vec.Vec2[f64] {
 	min_constraint := valid_angle(_min_constraint)
 	max_constraint := valid_angle(_max_constraint)
 	ab := b - a
 	bc := c - b
 	wanted_angle := valid_angle(ab.angle_between(bc))
-	abs_constraint := math.abs(valid_angle(constraint))
 	if wanted_angle > max_constraint {
 		return ab.unit().mul_scalar(bc.magnitude).rotate_around_ccw(origin, max_constraint)
 	} else if wanted_angle < min_constraint {
@@ -69,6 +92,6 @@ pub fn apply_min_max_angle_constraint(a vec.Vec2[f32], b vec.Vec2[f32], c vec.Ve
 }
 
 // returns the rad angle between -pi and pi
-pub fn valid_angle(angle f32) f32 {
+pub fn valid_angle(angle f64) f64 {
 	return math.mod(angle, 2 * math.pi) - math.pi
 }
