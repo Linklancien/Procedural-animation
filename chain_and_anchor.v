08471f216d2a0e1830f2e9d62d6c1f15b8ad800c
Linklancien <123779978+Linklancien@users.noreply.github.com>
2025-11-15 01:38:46 +0100

    Not working

--- chain_and_anchor.v
+++ chain_and_anchor.v
@@ -9,7 +9,7 @@ const origin = vec.vec2(f64(0.0), f64(0.0))
 
 // pos_constraints.len = points.len - 1 (0th is for the link between 0th and 1st point)
 // min/max_constraints.len = points.len - 2 (0th is for the 1st point)
-pub fn front_to_back_min_max(mut points []vec.Vec2[f64], pos_constraints []f64, min_constraints []f64, max_constraints []f64) {
+pub fn front_to_back_min_max(mut points []vec.Vec2[f64], pos_constraints []f64, min_constraints []f64, max_constraints []f64) {	
 	for ip in 0 .. points.len - 2 {
 		points[ip] = apply_pos_constraint(points[ip + 1], points[ip], pos_constraints[ip])
 		points[ip] = apply_min_max_angle_constraint(points[ip + 2], points[ip + 1], points[ip],
@@ -57,7 +57,7 @@ pub fn back_to_front(mut points []vec.Vec2[f64], pos_constraints []f64, angle_co
 
 // returns the position of b constrained to (b - a).magnitude = constraint
 pub fn apply_pos_constraint(a vec.Vec2[f64], b vec.Vec2[f64], constraint f64) vec.Vec2[f64] {
-	return a + b.unit().mul_scalar(constraint)
+	return a + (b - a).unit().mul_scalar(constraint)
 }
 
 // returns the furthest position for the new `c` point (but blocks the angle <`a` `b` `c`> at `constraint` (radians)
@@ -99,6 +99,31 @@ pub fn valid_angle(angle f64) f64 {
 }
 
 // Some algos:
-pub fn front_go_to(mut points []vec.Vec2[f64], pos_constraints []f64, angle_constraints []f64, target vec.Vec2[f64]){
+pub fn front_go_to(mut points []vec.Vec2[f64], pos_constraints []f64, angle_constraints []f64, target vec.Vec2[f64], repetition int){
+	for _ in 0 .. repetition{
+		points[0] = target
 
+		exclude_front_to_back(mut points, pos_constraints, angle_constraints)
+		println('pt: $points')
+	}
+}
+
+pub fn exclude_front_to_back(mut points []vec.Vec2[f64], pos_constraints []f64, angle_constraints []f64) {
+	for ip in 0 .. points.len - 2 {
+		 points[ip+1] = apply_pos_constraint(points[ip], points[ip + 1], pos_constraints[ip])
+		points[ip+1] = apply_angle_constraint(points[ip], points[ip + 1], points[ip + 2],
+			angle_constraints[ip])
+	}
+	points[points.len - 1] = apply_pos_constraint(points[points.len - 2], points[points.len - 1],
+		pos_constraints[points.len - 2])
+}
+
+pub fn extend_front_to_back_min_max(mut points []vec.Vec2[f64], pos_constraints []f64, min_constraints []f64, max_constraints []f64) {
+        for ip in 0 .. points.len - 2 {
+               
+                points[ip + 1] = apply_min_max_angle_constraint(points[ip], points[ip + 1], points[ip + 2],
+                        min_constraints[ip], max_constraints[ip])
+        }
+        points[points.len - 1] = apply_pos_constraint(points[points.len - 2], points[points.len - 1],
+                pos_constraints[points.len - 2])
 }
