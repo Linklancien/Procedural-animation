4c570bae52638ab7b8de9366edf458adb884c5ff
Linklancien <123779978+Linklancien@users.noreply.github.com>
2025-11-15 09:38:23 +0100

    fix angular_render + rename

--- graphic_debug/graphic_debug.v
+++ graphic_debug/graphic_debug.v
@@ -1,6 +1,7 @@
 module graphic_debug
 
 import gg
+import math { cos, sin }
 import math.vec
 
 // Render the basic shape of the array of point
@@ -14,12 +15,17 @@ pub fn basic_render(ctx gg.Context, points []vec.Vec2[f64], radius []f64, color 
 pub fn angular_render(ctx gg.Context, points []vec.Vec2[f64], radius []f64, color gg.Color) {
 	edges := 3
 	for i, point in points {
-		rotation := if i != points.len {
-			(points[i + 1] - point).angle()
+		rotation := if i != points.len - 1 {
+			(point-points[i + 1]).angle()
 		} else {
-			0
+			(points[i - 1] - point).angle()
 		}
-		ctx.draw_polygon_filled(f32(point.x), f32(point.y), f32(radius[i]), edges, f32(rotation),
-			color)
+		x := f32(point.x)
+		y := f32(point.y)
+		norm := 40
+		x2 := x + f32(norm*cos(rotation))
+		y2 := y + f32(norm*sin(rotation))
+		ctx.draw_polygon_filled(x, y, f32(radius[i]), edges, f32(rotation*180/math.pi), color)
+		ctx.draw_line(x, y, x2, y2, gg.red)
 	}
 }
