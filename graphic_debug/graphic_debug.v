f5b3cd92995d4c08235988647b654c5566b6b17e
Linklancien <123779978+Linklancien@users.noreply.github.com>
2026-01-12 22:59:54 +0100

    filled_render change location

--- graphic_debug/graphic_debug.v
+++ graphic_debug/graphic_debug.v
@@ -1,7 +1,6 @@
 module graphic_debug
 
 import gg
-import sokol.sgl
 import math { cos, sin }
 import math.vec
 
@@ -32,46 +31,3 @@ pub fn angular_render(ctx gg.Context, points []vec.Vec2[f64], radius []f64, colo
 	}
 }
 
-pub fn filled_render(ctx gg.Context, points []vec.Vec2[f64], radius []f64, c gg.Color){
-  if c.a != 255 {
-		sgl.load_pipeline(ctx.pipeline.alpha)
-	}
-	sgl.c4b(c.r, c.g, c.b, c.a)
-	sgl.begin_triangle_strip ()
-	
-	x0 := f32(points[0].x)
-	y0 := f32(points[0].y)
- 
-	rot := (points[0] - points[1]).angle()
-	angles := [-math.pi/6, math.pi/6, -math.pi/4, math.pi/4, -math.pi/2, math.pi/2]
-	for angle in angles{
-	  xc := x0 + f32(radius[0] * cos(rot + angle))
-		yc := y0 + f32(radius[0] * sin(rot + angle))
-		sgl.v2f(xc, yc)
-	}
-  
-	for i, point in points {
-		rotation := if i != points.len - 1 {
-			(point - points[i + 1]).angle()
-		} else {
-			(points[i - 1] - point).angle()
-		}
-		x := f32(point.x)
-		y := f32(point.y)
-		
-		x1 := x + f32(radius[i] * cos(rotation + math.pi/2))
-		y1:= y + f32(radius[i] * sin(rotation + math.pi/2))
-		x2 := x + f32(radius[i] * cos(rotation - math.pi/2))
-		y2 := y + f32(radius[i] * sin(rotation - math.pi/2))
-		
-    sgl.v2f(x1, y1)
-    sgl.v2f(x2, y2)
-  	
-    if i ==  points.len - 1{
-     	xf := x + f32(radius[i] * cos(rotation + math.pi))
-      yf := y + f32(radius[i] * sin(rotation + math.pi))
-      sgl.v2f(xf, yf)
-    }
-	}
-	sgl.end()
-}
