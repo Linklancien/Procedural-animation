58c3276058e72f373c2cbc9a88e04a5a1155fe5d
Linklancien <123779978+Linklancien@users.noreply.github.com>
2026-01-15 00:03:08 +0100

    for & match use

--- examples/fabrik_example.v
+++ examples/fabrik_example.v
@@ -125,38 +125,47 @@ pub fn arm_render(ctx gg.Context, points []vec.Vec2[f64], radius []f64, c gg.Col
 	}
 	sgl.c4b(c.r, c.g, c.b, c.a)
 	sgl.begin_triangle_strip()
-
-	x0 := f32(points[0].x)
-	y0 := f32(points[0].y)
-
-	rot := (points[0] - points[1]).angle()
-	angles := [-math.pi / 6, math.pi / 6, -math.pi / 4, math.pi / 4, -math.pi / 2, math.pi / 2]
-	for angle in angles {
-		xc := x0 + f32(radius[0] * cos(rot + angle))
-		yc := y0 + f32(radius[0] * sin(rot + angle))
-		sgl.v2f(xc, yc)
-	}
 	for i, point in points {
-		rotation := if i != points.len - 1 {
-			(point - points[i + 1]).angle()
-		} else {
-			(points[i - 1] - point).angle()
-		}
-		x := f32(point.x)
-		y := f32(point.y)
+		match i {
+			0 {
+				x0 := f32(point.x)
+				y0 := f32(point.y)
 
-		x1 := x + f32(radius[i] * cos(rotation + math.pi / 2))
-		y1 := y + f32(radius[i] * sin(rotation + math.pi / 2))
-		x2 := x + f32(radius[i] * cos(rotation - math.pi / 2))
-		y2 := y + f32(radius[i] * sin(rotation - math.pi / 2))
+				rot := (point - points[1]).angle()
+				angles := [math.pi / 6, -math.pi / 6, math.pi / 4, -math.pi / 4, math.pi / 2,
+					-math.pi / 2]
+				for angle in angles {
+					xc := x0 + f32(radius[0] * cos(rot + angle))
+					yc := y0 + f32(radius[0] * sin(rot + angle))
+					sgl.v2f(xc, yc)
+				}
+			}
+			// max {
+			//   println('max')
+			// }
+			else {
+				rotation := if i != points.len - 1 {
+					(point - points[i + 1]).angle()
+				} else {
+					(points[i - 1] - point).angle()
+				}
+				x := f32(point.x)
+				y := f32(point.y)
 
-		sgl.v2f(x1, y1)
-		sgl.v2f(x2, y2)
+				x1 := x + f32(radius[i] * cos(rotation + math.pi / 2))
+				y1 := y + f32(radius[i] * sin(rotation + math.pi / 2))
+				x2 := x + f32(radius[i] * cos(rotation - math.pi / 2))
+				y2 := y + f32(radius[i] * sin(rotation - math.pi / 2))
 
-		if i == points.len - 1 {
-			xf := x + f32(radius[i] * cos(rotation + math.pi))
-			yf := y + f32(radius[i] * sin(rotation + math.pi))
-			sgl.v2f(xf, yf)
+				sgl.v2f(x1, y1)
+				sgl.v2f(x2, y2)
+
+				if i == points.len - 1 {
+					xf := x + f32(radius[i] * cos(rotation + math.pi))
+					yf := y + f32(radius[i] * sin(rotation + math.pi))
+					sgl.v2f(xf, yf)
+				}
+			}
 		}
 	}
 	sgl.end()
