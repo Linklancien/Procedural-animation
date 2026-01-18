e1f8163b8c83f1387cf7819f2bff3d3d0f7b9c4b
Linklancien <123779978+Linklancien@users.noreply.github.com>
2026-01-18 19:01:16 +0100

    Spider (#16)
    * Create spider_walking.v
    
    * Update graphic_debug.v
    
    * Update spider_walking.v
    
    * Update fabrik_example.v
    
    * re write of all
    
    * ready

---
+++ examples/spider_walking.v
@@ -0,0 +1,183 @@
+import gg
+import linklancien.procanim
+import graphic_debug
+import math
+import math.vec
+
+const bg_color = gg.Color{}
+const render_debug = true
+
+struct App {
+mut:
+	ctx      &gg.Context = unsafe { nil }
+	text_cfg gg.TextCfg
+
+	x_mouse    int
+	y_mouse    int
+	win_width  int
+	win_height int
+
+	target vec.Vec2[f64]
+
+	spider Spider
+}
+
+fn main() {
+	mut app := &App{}
+	app.ctx = gg.new_context(
+		width:         app.win_width
+		height:        app.win_height
+		create_window: true
+		window_title:  '- Procedural Animation walking spider -'
+		user_data:     app
+		bg_color:      bg_color
+		frame_fn:      on_frame
+		init_fn:       on_init
+		event_fn:      on_event
+		sample_count:  2
+	)
+
+	app.ctx.run()
+}
+
+fn on_init(mut app App) {
+	size := app.ctx.window_size()
+	app.win_width = size.width
+	app.win_height = size.height
+
+	app.spider = Spider.create()
+}
+
+fn on_frame(mut app App) {
+	app.spider.update(app.target)
+	// Draw
+	app.ctx.begin()
+	app.spider.render(app.ctx)
+	app.ctx.end()
+}
+
+fn on_event(e &gg.Event, mut app App) {
+	size := app.ctx.window_size()
+	app.win_width = size.width
+	app.win_height = size.height
+
+	app.x_mouse, app.y_mouse = int(e.mouse_x), int(e.mouse_y)
+	app.target = vec.Vec2[f64]{
+		x: app.x_mouse
+		y: app.y_mouse
+	}
+	if e.char_code != 0 && e.char_code < 128 {
+		// app.change += u8(e.char_code).ascii_str()
+	}
+	match e.typ {
+		.key_down {
+			match e.key_code {
+				.f4 {
+					app.ctx.quit()
+				}
+				else {}
+			}
+		}
+		else {}
+	}
+}
+
+// Chain
+struct Chain {
+  color gg.Color
+	pos_constraints   []f64
+	angle_constraints []f64
+mut:
+	points []vec.Vec2[f64]
+}
+
+fn Chain.create_fixed(color gg.Color, len int, pos_constraint f64, angle_constraint f64) Chain {
+	return Chain{
+	  color: color
+		pos_constraints:   []f64{len: len, init: pos_constraint}
+		angle_constraints: []f64{len: len, init: angle_constraint}
+		points:            []vec.Vec2[f64]{len: len, init: vec.Vec2[f64]{
+			x: 1
+		}.mul_scalar(len - index)}
+	}
+}
+
+enum Updates_type {
+	goto
+	fabrik
+}
+
+fn (mut chain Chain) update(target vec.Vec2[f64], update Updates_type) {
+	match update {
+		.goto {
+			procanim.front_go_to(mut chain.points, chain.pos_constraints, chain.angle_constraints,
+				target, 1)
+		}
+		.fabrik {
+			procanim.fabrik(mut chain.points, chain.pos_constraints, chain.angle_constraints,
+				target, 1)
+		}
+	}
+}
+
+fn (chain Chain) render(ctx gg.Context, pos vec.Vec2[f64]) {
+  x := f32(pos.x)
+  y := f32(pos.y)
+	graphic_debug.filled_render_at(ctx, x, y, chain.points, chain.pos_constraints, chain.color)
+	if render_debug {
+		graphic_debug.basic_render_at(ctx, x, y, chain.points, chain.pos_constraints, gg.red)
+	}
+}
+
+// Spider
+struct Spider {
+mut:
+	legs []Leg
+	chain Chain
+}
+
+fn Spider.create() Spider {
+	len := 5
+	pos_constraint := 20.0
+	angle_constraint := math.pi * 2 / 6
+	ids := [1, 1, 4, 4]
+	return Spider{
+		legs:  []Leg{len: 4, init: Leg.create(ids[index])}
+		chain: Chain.create_fixed(gg.gray, len, pos_constraint, angle_constraint)
+	}
+}
+
+fn (mut spider Spider) update(target vec.Vec2[f64]) {
+	spider.chain.update(target, .goto)
+	for mut leg in mut spider.legs{
+	  leg.chain.update(target - spider.chain.points[leg.id_body_part], .fabrik)
+	}
+}
+
+fn (spider Spider) render(ctx gg.Context) {
+  spider.chain.render(ctx, vec.Vec2[f64]{})
+	for leg in spider.legs {
+		leg.render(ctx, spider.chain.points[leg.id_body_part])
+	}
+}
+
+// Leg
+struct Leg {
+mut:
+	id_body_part int
+	chain        Chain
+}
+
+fn Leg.create(id int) Leg {
+	len := 5
+	pos_constraint := 10.0
+	angle_constraint := math.pi * 2 / 6
+	return Leg{
+		chain:        Chain.create_fixed(gg.gray, len, pos_constraint, angle_constraint)
+		id_body_part: id
+	}
+}
+
+fn (leg Leg) render(ctx gg.Context, pos vec.Vec2[f64]) {
+	leg.chain.render(ctx, pos)
+}
--- graphic_debug/graphic_debug.v
+++ graphic_debug/graphic_debug.v
@@ -1,6 +1,7 @@
 module graphic_debug
 
 import gg
+import sokol.sgl
 import math { cos, sin }
 import math.vec
 
@@ -35,4 +36,84 @@ pub fn angular_render(ctx gg.Context, points []vec.Vec2[f64], radius []f64, colo
 			color)
 		ctx.draw_line(x, y, x2, y2, gg.red)
 	}
+}
+
+// Complex_render
+pub fn filled_render(ctx gg.Context, points []vec.Vec2[f64], radius []f64, c gg.Color) {
+  filled_render_at(ctx, 0.0, 0.0, points, radius, c)
+}
+  
+pub fn filled_render_at(ctx gg.Context, x_abs f32, y_abs f32, points []vec.Vec2[f64], radius []f64, c gg.Color) {
+	if c.a != 255 {
+		sgl.load_pipeline(ctx.pipeline.alpha)
+	}
+	sgl.c4b(c.r, c.g, c.b, c.a)
+	sgl.begin_triangle_strip()
+
+	max := points.len - 1
+	for i in 0 .. points.len {
+		rotation := if i == 0 {
+			(points[i] - points[i + 1]).angle()
+		} else if i == max {
+			(points[i - 1] - points[i]).angle()
+		} else {
+			(points[i - 1] - points[i + 1]).angle()
+		}
+		x := f32(points[i].x) + x_abs
+		y := f32(points[i].y) + y_abs
+		match i {
+			0 {
+				add_points_in_arc(x, y, f32(radius[i]), math.pi / 2, f32(rotation),
+					true, 2)
+				add_opposing_points(x, y, f32(radius[i]), rotation)
+			}
+			max {
+				add_opposing_points(x, y, f32(radius[i]), rotation)
+				add_points_in_arc(x, y, f32(radius[i]), math.pi / 2, f32(rotation + math.pi),
+					false, 2)
+			}
+			else {
+				add_opposing_points(x, y, f32(radius[i]), rotation)
+			}
+		}
+	}
+	sgl.end()
+}
+
+fn add_opposing_points(x f32, y f32, radius f32, rotation f64) {
+	x1, y1, x2, y2 := get_opposing_pos(x, y, radius, rotation)
+
+	sgl.v2f(x1, y1)
+	sgl.v2f(x2, y2)
+}
+
+fn add_points_in_arc(x f32, y f32, radius f32, extreme_angle f32, rota f32, from_the_rota bool, step int) {
+	f := if from_the_rota {
+		fn [step, extreme_angle, rota] (nth_step int) f32 {
+			return extreme_angle * f32(nth_step) / f32(step)
+		}
+	} else {
+		fn [step, extreme_angle, rota] (nth_step int) f32 {
+			return extreme_angle - extreme_angle * f32(nth_step) / f32(step)
+		}
+	}
+	for i in 0 .. (step + 1) {
+		angle := f(i)
+		xa := x + f32(radius * cos(rota + angle))
+		ya := y + f32(radius * sin(rota + angle))
+
+		sgl.v2f(xa, ya)
+		xo := x + f32(radius * cos(rota - angle))
+		yo := y + f32(radius * sin(rota - angle))
+
+		sgl.v2f(xo, yo)
+	}
+}
+
+fn get_opposing_pos(x f32, y f32, radius f32, rotation f64) (f32, f32, f32, f32) {
+	x1 := x + f32(radius * cos(rotation + math.pi / 2))
+	y1 := y + f32(radius * sin(rotation + math.pi / 2))
+	x2 := x + f32(radius * cos(rotation - math.pi / 2))
+	y2 := y + f32(radius * sin(rotation - math.pi / 2))
+	return x1, y1, x2, y2
 }
