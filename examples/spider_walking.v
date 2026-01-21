c5ac03e8eea6684b9543048df920d40efa34ef0d
Linklancien <123779978+Linklancien@users.noreply.github.com>
2026-01-21 23:24:11 +0100

    cleanup + fmt

--- examples/spider_walking.v
+++ examples/spider_walking.v
@@ -151,25 +151,25 @@ fn Spider.create() Spider {
 }
 
 fn (mut spider Spider) update(head_target vec.Vec2[f64]) {
-	// iniitialise the update:
-	for mut leg in mut spider.legs {
-	  // get the new target relatively to the leg base
-		leg.current_target = leg.absolute_target - spider.chain.points[leg.id_body_part]
-	}
+	// // iniitialise the update:
+	// for mut leg in mut spider.legs {
+	//   // get the new target relatively to the leg base
+	// 	leg.current_target = leg.absolute_target - spider.chain.points[leg.id_body_part]
+	// }
 	// start moving things
 	spider.chain.update(head_target, .goto)
 	// ofset := [10, -10, 0, 0]
 	turn := [true, false, true, false]
 	for i, mut leg in mut spider.legs {
-		radius := 10
-		leg.absolute_target = leg.current_target + spider.chain.points[leg.id_body_part]
-		if (leg.current_target).magnitude() > radius {
-			println('passed')
-			check_at := spider.chain.points[leg.id_body_part] + leg.chain.points[0]
+		radius := 50
+		leg.current_target = leg.absolute_target - spider.chain.points[leg.id_body_part]
+		if leg.current_target.magnitude() > radius {
+			check_at := spider.chain.points[leg.id_body_part]
 			if target := get_target(check_at.x, check_at.y, radius, turn[i], calc_surface) {
 				leg.absolute_target = target
 			}
-			leg.current_target -= spider.chain.points[leg.id_body_part]
+			// get the new target relatively to the leg base
+			leg.current_target = leg.absolute_target - spider.chain.points[leg.id_body_part]
 		}
 		leg.chain.update(leg.current_target, .fabrik)
 	}
